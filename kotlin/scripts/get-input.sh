2d06badd9f5148e51a66a04ec49524160fa3412e
Stefan Scheidt <stefan.scheidt@rewe-digital.com>
2022-12-01 17:22:13 +0100

    Update templates

--- kotlin/scripts/get-input.sh
+++ kotlin/scripts/get-input.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 YEAR=$1
 DAY=$2
 
-printf -v DEST "./year%s/input" "$YEAR"
+printf -v DEST "./year%s/src/main/resources/input" "$YEAR"
 printf -v FILE "%s/day%02d.txt" "$DEST" "$DAY"
 
 mkdir -p "$DEST"
--- kotlin/scripts/init-day.sh
+++ kotlin/scripts/init-day.sh
@@ -7,9 +7,17 @@ printf -v DAY "%02d" "$2"
 MODULE="year$YEAR"
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
-printf -v DEST "%s/src/test/kotlin/day%s/Day%sTest.kt" "$MODULE" "$DAY" "$DAY"
+printf -v DEST_MAIN "%s/src/main/kotlin/day%s/Day%s.kt" "$MODULE" "$DAY" "$DAY"
+
+mkdir -p "$MODULE/src/main/kotlin/day$DAY"
+sed \
+  -e "s/%%DAY%%/$DAY/g" \
+  "$SCRIPT_DIR/templates/Day.kt.template" > "$DEST_MAIN"
+
+
+printf -v DEST_TEST "%s/src/test/kotlin/day%s/Day%sTest.kt" "$MODULE" "$DAY" "$DAY"
 
 mkdir -p "$MODULE/src/test/kotlin/day$DAY"
 sed \
   -e "s/%%DAY%%/$DAY/g" \
-  "$SCRIPT_DIR/templates/DayTest.kt.template" > "$DEST"
+  "$SCRIPT_DIR/templates/DayTest.kt.template" > "$DEST_TEST"
