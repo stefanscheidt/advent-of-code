eeb177f0abc77aefc7940835c84c43bb953c4abe
Stefan Scheidt <stefan.scheidt@rewe-digital.com>
2022-12-28 21:52:32 +0100

    Solve day 19 of 2022

---
+++ kotlin/year2022/src/main/kotlin/day19/Day19.kt
@@ -0,0 +1,145 @@
+package day19
+
+import common.io.inputFile
+import day19.Mineral.Clay
+import day19.Mineral.Geode
+import day19.Mineral.Obsidian
+import day19.Mineral.Ore
+import java.io.File
+import java.util.PriorityQueue
+import kotlin.math.ceil
+
+
+val file = inputFile("day19.txt")
+
+fun main() {
+    val solution = solvePuzzle(file)
+
+    println("1: ${solution.first}")
+    println("2: ${solution.second}")
+}
+
+fun solvePuzzle(file: File): Pair<Int, Int> {
+    val blueprints = parseInput(file.readLines())
+    return Pair(solvePartOne(blueprints), solvePartTwo(blueprints))
+}
+
+fun parseInput(input: List<String>): List<Blueprint> =
+    input.map(::parseBlueprint)
+
+fun parseBlueprint(input: String): Blueprint {
+    val parts = input.split(" ")
+    return Blueprint(
+        mapOf(
+            Ore to mapOf(Ore to parts[6].toInt()),
+            Clay to mapOf(Ore to parts[12].toInt()),
+            Obsidian to mapOf(Ore to parts[18].toInt(), Clay to parts[21].toInt()),
+            Geode to mapOf(Ore to parts[27].toInt(), Obsidian to parts[30].toInt())
+        )
+    )
+}
+
+fun solvePartOne(blueprints: List<Blueprint>): Int =
+    blueprints.mapIndexed { index, blueprint ->
+        (index + 1) * blueprint.qualityLevel(targetMineral = Geode, timeLimit = 24)
+    }.sum()
+
+fun solvePartTwo(blueprints: List<Blueprint>): Int =
+    blueprints.take(3).map {
+        it.qualityLevel(targetMineral = Geode, timeLimit = 32)
+    }.reduce(Int::times)
+
+enum class Mineral {
+    Ore, Clay, Obsidian, Geode
+}
+
+data class Blueprint(val costsForRobots: Map<Mineral, Map<Mineral, Int>>) {
+    fun maxCost(mineral: Mineral): Int =
+        costsForRobots.values.maxOf { it[mineral] ?: 0 }
+
+    fun qualityLevel(targetMineral: Mineral, timeLimit: Int): Int {
+        var qualityLevel = 0
+        val queue = PriorityQueue<Stock>()
+        queue.add(Stock(this, targetMineral))
+        while (queue.isNotEmpty()) {
+            val stock = queue.poll()
+            qualityLevel = maxOf(qualityLevel, stock.amountOfMineral(targetMineral))
+            if (stock.targetMineralUpperBound(timeLimit) > qualityLevel) {
+                queue.addAll(stock.possibleFutureStocks(timeLimit))
+            }
+        }
+        return qualityLevel
+    }
+}
+
+data class Stock(
+    val blueprint: Blueprint,
+    val targetMineral: Mineral,
+    val time: Int = 1,
+    val robots: Map<Mineral, Int> = mapOf(Ore to 1, Clay to 0, Obsidian to 0, Geode to 0),
+    val minerals: Map<Mineral, Int> = mapOf(Ore to 1, Clay to 0, Obsidian to 0, Geode to 0),
+) : Comparable<Stock> {
+
+    fun amountOfMineral(mineral: Mineral): Int =
+        minerals[mineral] ?: 0
+
+    fun targetMineralUpperBound(timeLimit: Int): Int {
+        val timeLeft = timeLimit - time
+        val numberOfRobots = robots[targetMineral] ?: 0
+        return amountOfMineral(targetMineral) + (numberOfRobots..(numberOfRobots + timeLeft)).sum()
+    }
+
+    fun possibleFutureStocks(timeLimit: Int): List<Stock> {
+        val result = mutableListOf<Stock>()
+        if (time < timeLimit) {
+            Mineral.values().forEach { mineralToCollect ->
+                if (robotShouldBeBuilt(mineralToCollect)) {
+                    val futureStock = stockAfterRobotWasBuilt(mineralToCollect)
+                    if (futureStock.time <= timeLimit) result.add(futureStock)
+                }
+            }
+        }
+        return result
+    }
+
+    private fun mineralsToBuildRobotAreOnStock(mineralToCollect: Mineral): Boolean =
+        blueprint.costsForRobots[mineralToCollect]?.map { it.key }?.all { (minerals[it] ?: 0) > 0 } ?: true
+
+    private fun robotIsNeeded(mineralToCollect: Mineral): Boolean =
+        mineralToCollect == targetMineral || (robots[mineralToCollect] ?: 0) < blueprint.maxCost(mineralToCollect)
+
+    private fun robotShouldBeBuilt(mineralToCollect: Mineral): Boolean {
+        return robotIsNeeded(mineralToCollect) && mineralsToBuildRobotAreOnStock(mineralToCollect)
+    }
+
+    private fun minutesNeededToBuildRobot(blueprint: Blueprint, mineralToCollect: Mineral): Int {
+        val max = blueprint.costsForRobots[mineralToCollect]
+            ?.maxOf { (mineral, cost) ->
+                val mineralOnStock = minerals[mineral] ?: 0
+                val numberOfRobots = robots[mineral] ?: 0
+                if (mineralOnStock >= cost) 0
+                else ceil((cost - mineralOnStock).toFloat() / numberOfRobots.toFloat()).toInt()
+            } ?: 0
+        return max + 1
+    }
+
+    private fun stockAfterRobotWasBuilt(mineralToCollect: Mineral): Stock {
+        val minutesNeeded = minutesNeededToBuildRobot(blueprint, mineralToCollect)
+        val stock = Stock(
+            blueprint = blueprint,
+            targetMineral = targetMineral,
+            time = time + minutesNeeded,
+            robots = robots.mapValues { (mineral, amount) ->
+                if (mineral == mineralToCollect) amount + 1 else amount
+            },
+            minerals = minerals.mapValues { (mineral, amount) ->
+                val cost = blueprint.costsForRobots[mineralToCollect]?.get(mineral) ?: 0
+                amount - cost + minutesNeeded * (robots[mineral] ?: 0)
+            }
+        )
+        return stock
+    }
+
+    override fun compareTo(other: Stock): Int =
+        other.amountOfMineral(other.targetMineral).compareTo(amountOfMineral(targetMineral))
+}
---
+++ kotlin/year2022/src/test/kotlin/day19/Day19Test.kt
@@ -0,0 +1,56 @@
+package day19
+
+import day19.Mineral.Clay
+import day19.Mineral.Geode
+import day19.Mineral.Obsidian
+import day19.Mineral.Ore
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+class Day19Test {
+
+    private val blueprint1 = "Blueprint 1:" +
+            " Each ore robot costs 4 ore." +
+            " Each clay robot costs 2 ore." +
+            " Each obsidian robot costs 3 ore and 14 clay." +
+            " Each geode robot costs 2 ore and 7 obsidian."
+
+    private val blueprint2 = "Blueprint 2:" +
+            " Each ore robot costs 2 ore." +
+            " Each clay robot costs 3 ore." +
+            " Each obsidian robot costs 3 ore and 8 clay." +
+            " Each geode robot costs 3 ore and 12 obsidian."
+
+    private val sample = listOf(blueprint1, blueprint2)
+
+    @Test
+    fun `parse blueprint`() {
+        parseBlueprint(blueprint1) shouldBe Blueprint(
+            mapOf(
+                Ore to mapOf(Ore to 4),
+                Clay to mapOf(Ore to 2),
+                Obsidian to mapOf(Ore to 3, Clay to 14),
+                Geode to mapOf(Ore to 2, Obsidian to 7)
+            )
+        )
+    }
+
+    @Test
+    fun `solve part one with sample input`() {
+        solvePartOne(parseInput(sample)) shouldBe 33
+    }
+
+    @Test
+    fun `solve part two with sample input`() {
+        solvePartTwo(parseInput(sample)) shouldBe 56 * 62
+    }
+
+    @Test
+    fun `solve puzzle`() {
+        val solution = solvePuzzle(file)
+
+        solution.first shouldBe 1624
+        solution.second shouldBe 12628
+    }
+
+}
