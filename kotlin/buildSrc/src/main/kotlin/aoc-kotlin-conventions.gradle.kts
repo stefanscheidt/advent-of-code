7eb64e8d4f1b95bd9741c2396e0035b2e5bdeb4d
Stefan Scheidt <stefan.scheidt@rewe-digital.com>
2023-11-11 18:46:25 +0100

    Update dependencies

--- kotlin/buildSrc/src/main/kotlin/aoc-kotlin-conventions.gradle.kts
+++ kotlin/buildSrc/src/main/kotlin/aoc-kotlin-conventions.gradle.kts
@@ -28,15 +28,16 @@ dependencies {
     implementation("com.github.h0tk3y.betterParse:better-parse:0.4.4")
 
     // https://junit.org/junit5/docs/current/user-guide/
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation(platform("org.junit:junit-bom:5.10.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
     // https://kotlinlang.org/api/latest/kotlin.test/kotlin.test/
     testImplementation(kotlin("test"))
 
     // https://kotest.io/docs/assertions/assertions.html
-    testImplementation("io.kotest:kotest-assertions-core:5.5.4")
+    testImplementation("io.kotest:kotest-assertions-core:5.8.0")
 
     // https://mockk.io/
-    testImplementation("io.mockk:mockk:1.13.2")
+    testImplementation("io.mockk:mockk:1.13.8")
 }
