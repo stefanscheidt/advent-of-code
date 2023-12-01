a05285b5206f445d5a383d8323ce14dc47867856
Stefan Scheidt <stefan.scheidt@rewe-digital.com>
2023-12-01 08:55:19 +0100

    Solve day 1

--- kotlin/year2023/src/main/kotlin/day01/Day01.kt
+++ kotlin/year2023/src/main/kotlin/day01/Day01.kt
@@ -13,10 +13,48 @@ fun main() {
     println("2: ${solution.second}")
 }
 
-fun solvePuzzle(file: File): Pair<String, String> {
-    return Pair("", "")
+fun solvePuzzle(file: File): Pair<Int, Int> {
+    val input = file.readLines()
+    return Pair(
+        calibrationValue(input),
+        calibrationValue(input.map(::replaceNumerals))
+    )
 }
 
-fun parseInput(input: String): Any {
-    TODO()
+fun calibrationValue(line: String): Int {
+    val firstDigit = line.first { it.isDigit() }
+    val lastDigit = line.last { it.isDigit() }
+    return "$firstDigit$lastDigit".toInt()
+}
+
+fun calibrationValue(input: List<String>): Int {
+    return input.sumOf { calibrationValue(it) }
+}
+
+val numerals = mapOf(
+    "one" to "1",
+    "oneight" to "18",
+    "two" to "2",
+    "twone" to "21",
+    "three" to "3",
+    "threeight" to "38",
+    "four" to "4",
+    "five" to "5",
+    "fiveight" to "58",
+    "six" to "6",
+    "seven" to "7",
+    "sevenine" to "79",
+    "eight" to "8",
+    "eightwo" to "82",
+    "eighthree" to "83",
+    "nine" to "9",
+    "nineight" to "98",
+)
+
+fun replaceNumerals(line: String): String {
+    return numerals.entries
+        .sortedByDescending { it.key.length }
+        .fold(line) { current, entry ->
+            current.replace(entry.key, entry.value)
+        }
 }
--- kotlin/year2023/src/test/kotlin/day01/Day01Test.kt
+++ kotlin/year2023/src/test/kotlin/day01/Day01Test.kt
@@ -5,25 +5,44 @@ import org.junit.jupiter.api.Test
 
 class Day01Test {
 
-    private val sample = """
+    private val sample1 = """
+        1abc2
+        pqr3stu8vwx
+        a1b2c3d4e5f
+        treb7uchet
+    """.trimIndent()
+
+    private val sample2 = """
+        two1nine
+        eightwothree
+        abcone2threexyz
+        xtwone3four
+        4nineeightseven2
+        zoneight234
+        7pqrstsixteen
     """.trimIndent()
 
     @Test
     fun `solve part one with sample input`() {
-        TODO()
+        calibrationValue(sample1.lines()) shouldBe 142
+    }
+
+    @Test
+    fun `replace number words`() {
+        calibrationValue(replaceNumerals("eightwo")) shouldBe 82
     }
 
     @Test
     fun `solve part two with sample input`() {
-        TODO()
+        calibrationValue(sample2.lines().map(::replaceNumerals)) shouldBe 281
     }
 
     @Test
     fun `solve puzzle`() {
         val solution = solvePuzzle(file)
 
-        solution.first shouldBe ""
-        solution.second shouldBe ""
+        solution.first shouldBe 54877
+        solution.second shouldBe 54100
     }
 
 }
