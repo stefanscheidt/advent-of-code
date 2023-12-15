4b433481dd5a3692f7dd80a7812f0c1fdf840f34
Stefan Scheidt <stefan.scheidt@rewe-digital.com>
2023-12-15 18:01:12 +0100

    Solve day 15
    Co-authored-by: Marc Gidaszewski <marc.gidaszewski@rewe-digital.com>
    Co-authored-by: Simon Moss <simon.moss@rewe-group.com>

---
+++ kotlin/year2023/src/main/kotlin/day15/Day15.kt
@@ -0,0 +1,95 @@
+package day15
+
+import common.io.inputFile
+import java.io.File
+
+
+val file = inputFile("day15.txt")
+
+fun main() {
+    val solution = solvePuzzle(file)
+
+    println("1: ${solution.first}")
+    println("2: ${solution.second}")
+}
+
+fun parseInput(input: String): List<String> =
+    input.replace("\n", "").trim(',').split(',')
+
+fun solvePuzzle(file: File): Pair<String, String> {
+    val input = parseInput(file.readText().trim())
+    val commands = input.map(::parseCommand)
+    return Pair("${solvePartOne(input)}", "${solvePartTwo(commands)}")
+}
+
+fun solvePartOne(input: List<String>): Int =
+    input.sumOf { it.holidayHashCode() }
+
+fun solvePartTwo(commands: List<Command>): Int {
+    val boxes = List(256) { _ -> mutableListOf<Lens>() }
+    boxes.apply(commands)
+    return boxes.focusingPower
+}
+
+fun String.holidayHashCode(): Int =
+    fold(0) { acc, c -> (acc + c.code) * 17 % 256 }
+
+sealed interface Command {
+    val label: String
+}
+
+data class AddOrReplace(override val label: String, val focalLength: Int) : Command
+
+data class Remove(override val label: String) : Command
+
+fun parseCommand(input: String): Command {
+    fun err(): Nothing = error("invalid command [$input]")
+
+    val commandRegex = """(?<label>\w+)(?<operation>[-=])(?<focalLength>\d)?""".toRegex()
+    val match = commandRegex.matchEntire(input) ?: err()
+    val label = match.groups["label"]?.value ?: err()
+    val operation = match.groups["operation"]?.value ?: err()
+    val focalLength = match.groups["focalLength"]?.value?.toInt()
+    return when (operation) {
+        "=" -> AddOrReplace(label, focalLength ?: err())
+        "-" -> Remove(label)
+        else -> err()
+    }
+}
+
+data class Lens(val label: String, val focalLength: Int)
+
+typealias Box = MutableList<Lens>
+
+fun Box.apply(command: Command) {
+    when (command) {
+        is AddOrReplace -> {
+            val index = indexOfFirst { it.label == command.label }
+            val lens = Lens(command.label, command.focalLength)
+            if (index == -1) {
+                this.add(lens)
+            } else {
+                this[index] = lens
+            }
+        }
+
+        is Remove -> {
+            val index = indexOfFirst { it.label == command.label }
+            if (index != -1) removeAt(index)
+        }
+    }
+}
+
+fun List<Box>.apply(commands: List<Command>) {
+    commands.forEach { command ->
+        val box = this[command.label.holidayHashCode()]
+        box.apply(command)
+    }
+}
+
+val List<Box>.focusingPower: Int
+    get() = flatMapIndexed { boxIndex, box ->
+        box.mapIndexed { lensIndex, lens ->
+            (boxIndex + 1) * (lensIndex + 1) * lens.focalLength
+        }
+    }.sum()
---
+++ kotlin/year2023/src/test/kotlin/day15/Day15Test.kt
@@ -0,0 +1,77 @@
+package day15
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+class Day15Test {
+
+    private val sample = """
+        rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7
+    """.trimIndent()
+
+    @Test
+    fun `parse ignores new lines and trailing commans`() {
+        val input = """
+            ABC,
+            DEF,
+        """.trimIndent()
+
+        parseInput(input) shouldBe listOf("ABC", "DEF")
+    }
+
+    @Test
+    fun `compute hash of HASH`() {
+        "HASH".holidayHashCode() shouldBe 52
+    }
+
+    @Test
+    fun `parse a command`() {
+        parseCommand("rn=1") shouldBe AddOrReplace("rn", 1)
+        parseCommand("cm-") shouldBe Remove("cm")
+    }
+
+    @Test
+    fun `apply add or replace`() {
+        val box = mutableListOf<Lens>()
+        box.apply(AddOrReplace("rn", 1))
+
+        box shouldBe mutableListOf(Lens("rn", 1))
+
+        box.apply(AddOrReplace("rn", 2))
+
+        box shouldBe mutableListOf(Lens("rn", 2))
+    }
+
+    @Test
+    fun `apply remove command`() {
+        val box = mutableListOf(Lens("aa", 1), Lens("bb", 2), Lens("cc", 3))
+
+        box.apply(Remove("xx"))
+
+        box.size shouldBe 3
+
+        box.apply(Remove("bb"))
+
+        box shouldBe mutableListOf(Lens("aa", 1), Lens("cc", 3))
+    }
+
+    @Test
+    fun `solve part one with sample input`() {
+        solvePartOne(parseInput(sample)) shouldBe 1320
+    }
+
+    @Test
+    fun `solve part two with sample input`() {
+        val commands = parseInput(sample).map(::parseCommand)
+        solvePartTwo(commands) shouldBe 145
+    }
+
+    @Test
+    fun `solve puzzle`() {
+        val solution = solvePuzzle(file)
+
+        solution.first shouldBe "503487"
+        solution.second shouldBe "261505"
+    }
+
+}
