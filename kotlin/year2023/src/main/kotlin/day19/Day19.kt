1d05ee54fb193ede535e97ac8ba721e59853970c
Stefan Scheidt <stefan.scheidt@rewe-digital.com>
2023-12-19 23:29:54 +0100

    Solve day 19 part 1

---
+++ kotlin/year2023/src/main/kotlin/day19/Day19.kt
@@ -0,0 +1,109 @@
+package day19
+
+import common.io.inputFile
+import java.io.File
+
+
+val file = inputFile("day19.txt")
+
+fun main() {
+    val solution = solvePuzzle(file)
+
+    println("1: ${solution.first}")
+    println("2: ${solution.second}")
+}
+
+fun solvePuzzle(file: File): Pair<String, String> {
+    val (workflows, ratings) = parseInput(file.readText().trim())
+
+    return Pair("${solvePartOne(workflows, ratings)}", "")
+}
+
+fun solvePartOne(workflows: Map<String, Workflow>, ratings: List<Rating>): Int =
+    ratings.map { it to workflows.assess(it) }
+        .filter { (_, assessment) -> assessment is Accepted }
+        .sumOf { (rating, _) -> rating.a + rating.m + rating.s + rating.x }
+
+fun parseInput(input: String): Pair<Map<String, Workflow>, List<Rating>> {
+    val (workflowInput, ratingInput) = input.split("""\s+\n""".toRegex()).map(String::lines)
+
+    val workflows = workflowInput.map(::parseWorkflow).associateBy { it.name }
+    val ratings = ratingInput.map(::parseRating)
+
+    return Pair(workflows, ratings)
+}
+
+val ratingRegex = """^\{x=(\d+),m=(\d+),a=(\d+),s=(\d+)}$""".toRegex()
+
+fun parseRating(input: String): Rating {
+    val (x, m, a, s) = ratingRegex.matchEntire(input)?.destructured ?: error("invalid rating [$input]")
+    return Rating(x.toInt(), m.toInt(), a.toInt(), s.toInt())
+}
+
+val workflowRegex = """^(\w+)\{(\S*)}$""".toRegex()
+
+fun parseWorkflow(input: String): Workflow {
+    val (name, rulesInput) = workflowRegex.matchEntire(input)?.destructured ?: error("invalid workflow [$input]")
+    return Workflow(name, rulesInput.split(",").map(::parseRule))
+}
+
+fun parseRule(input: String): Rule {
+    if (':' !in input) return { _ -> input.toAssessment() }
+
+    val (predicate, result) = input.split(":")
+    val getter = predicate[0].toGetter()
+    val comparator = predicate[1].toComparator()
+    val value = predicate.drop(2).toInt()
+
+    return { rating ->
+        if (comparator.compare(getter(rating), value) > 0) result.toAssessment() else null
+    }
+}
+
+
+fun Char.toGetter(): (Rating) -> Int =
+    when (this) {
+        'x' -> { r -> r.x }
+        'm' -> { r -> r.m }
+        'a' -> { r -> r.a }
+        's' -> { r -> r.s }
+        else -> error("invalid property name [$this]")
+    }
+
+fun Char.toComparator(): Comparator<Int> =
+    when (this) {
+        '>' -> Comparator { i1, i2 -> i1.compareTo(i2) }
+        '<' -> Comparator { i1, i2 -> i2.compareTo(i1) }
+        else -> error("invalid comparison operator [$this]")
+    }
+
+fun String.toAssessment(): Assessment =
+    when (this) {
+        "A" -> Accepted
+        "R" -> Rejected
+        else -> Reference(this)
+    }
+
+data class Rating(val x: Int = 0, val m: Int = 0, val a: Int = 0, val s: Int = 0)
+
+sealed class Assessment
+data class Reference(val name: String) : Assessment()
+data object Accepted : Assessment()
+data object Rejected : Assessment()
+
+typealias Rule = (Rating) -> Assessment?
+
+data class Workflow(val name: String, val rules: List<Rule>) {
+    fun assess(rating: Rating): Assessment =
+        rules.asSequence().map { it(rating) }.first { it != null } ?: Rejected
+}
+
+fun Map<String, Workflow>.assess(rating: Rating): Assessment {
+    val start = get("in") ?: error("no workflow named 'in'")
+    var assessment = start.assess(rating)
+    while (assessment is Reference) {
+        val workflow = get(assessment.name) ?: error("invalid workflow name [${assessment.name}]")
+        assessment = workflow.assess(rating)
+    }
+    return assessment
+}
---
+++ kotlin/year2023/src/test/kotlin/day19/Day19Test.kt
@@ -0,0 +1,71 @@
+package day19
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+class Day19Test {
+
+    private val sample = """
+        px{a<2006:qkq,m>2090:A,rfg}
+        pv{a>1716:R,A}
+        lnx{m>1548:A,A}
+        rfg{s<537:gd,x>2440:R,A}
+        qs{s>3448:A,lnx}
+        qkq{x<1416:A,crn}
+        crn{x>2662:A,R}
+        in{s<1351:px,qqz}
+        qqz{s>2770:qs,m<1801:hdj,R}
+        gd{a>3333:R,R}
+        hdj{m>838:A,pv}
+
+        {x=787,m=2655,a=1222,s=2876}
+        {x=1679,m=44,a=2067,s=496}
+        {x=2036,m=264,a=79,s=2244}
+        {x=2461,m=1339,a=466,s=291}
+        {x=2127,m=1623,a=2188,s=1013}
+    """.trimIndent()
+
+    @Test
+    fun `parse rule`() {
+        val rule1 = parseRule("rfg")
+        rule1(Rating()) shouldBe Reference("rfg")
+
+        val rule2 = parseRule("m>2090:A")
+        rule2(Rating(m = 2090)) shouldBe null
+        rule2(Rating(m = 2091)) shouldBe Accepted
+
+        val rule3 = parseRule("a<2006:qkq")
+        rule3(Rating(a = 2005)) shouldBe Reference("qkq")
+        rule3(Rating(a = 2006)) shouldBe null
+    }
+
+    @Test
+    fun `assess rating`() {
+        val workflow = parseWorkflow("px{a<2006:qkq,m>2090:A,rfg}")
+
+        workflow.assess(Rating(a = 0, m = 0)) shouldBe Reference("qkq")
+        workflow.assess(Rating(a = 2006, m = 2091)) shouldBe Accepted
+        workflow.assess(Rating(a = 2006, m = 2090)) shouldBe Reference("rfg")
+    }
+
+    @Test
+    fun `solve part one with sample input`() {
+        val (workflows, ratings) = parseInput(sample)
+
+        solvePartOne(workflows, ratings) shouldBe 19114
+    }
+
+    @Test
+    fun `solve part two with sample input`() {
+        TODO()
+    }
+
+    @Test
+    fun `solve puzzle`() {
+        val solution = solvePuzzle(file)
+
+        solution.first shouldBe "368964"
+        solution.second shouldBe ""
+    }
+
+}
