460b31e1885c9d6be2a6bbc8c41fb4dab68e457f
Stefan Scheidt <stefan.scheidt@rewe-digital.com>
2023-12-23 22:24:48 +0100

    Solve day 22

---
+++ kotlin/year2023/src/main/kotlin/day22/Day22.kt
@@ -0,0 +1,112 @@
+package day22
+
+import common.io.inputFile
+import java.io.File
+
+
+val file = inputFile("day22.txt")
+
+fun main() {
+    val solution = solvePuzzle(file)
+
+    println("1: ${solution.first}")
+    println("2: ${solution.second}")
+}
+
+fun solvePuzzle(file: File): Pair<String, String> {
+    val bricks = parseBricks(file.readLines().filter(String::isNotBlank))
+    return Pair("${solvePartOne(bricks)}", "${solvePartTwo(bricks)}")
+}
+
+fun solvePartOne(bricks: List<Brick>): Int =
+    bricks.settled().count { it.isSafeToDesintegrate() }
+
+fun solvePartTwo(bricks: List<Brick>): Int {
+    val settled = bricks.settled()
+    return settled
+        .filter { !it.isSafeToDesintegrate() }
+        .parallelStream()
+        .map { it.disintegrate(settled).size }
+        .reduce(0, Int::plus)
+}
+
+fun parseBrick(input: String): Brick {
+    val (fst, snd) = input.split("~").map { it.split(",").map(String::toInt) }
+    return Brick(
+        xs = (fst[0]..snd[0]),
+        ys = (fst[1]..snd[1]),
+        zs = (fst[2]..snd[2])
+    )
+}
+
+fun parseBricks(input: List<String>): List<Brick> =
+    input.map(::parseBrick).sortedBy { it.zs.first }
+
+data class Brick(val xs: IntRange, val ys: IntRange, val zs: IntRange) {
+
+    val supporting = mutableSetOf<Brick>()
+    val supportedBy = mutableSetOf<Brick>()
+
+    infix fun willSupport(other: Brick): Boolean =
+        this overlapps other && zs.last < other.zs.first - 1
+
+    infix fun supports(other: Brick): Boolean =
+        this overlapps other && zs.last == other.zs.first - 1
+
+    fun moveTo(z: Int): Brick =
+        copy(zs = zs.moveTo(z))
+
+    fun addSupporter(brick: Brick) {
+        if (brick supports this) {
+            brick.supporting.add(this)
+            supportedBy.add(brick)
+        }
+    }
+
+    fun isSafeToDesintegrate(): Boolean =
+        supporting.all { it.supportedBy.size > 1 }
+
+    /**
+     * @return Subset of passed in bricks that will fall when this gets disintegrated
+     */
+    fun disintegrate(bricks: List<Brick>): Set<Brick> {
+        tailrec fun go(bricks: Set<Brick>, others: Set<Brick>): Set<Brick> {
+            val unsupported = others.filter { brick -> brick.zs.first > 1 && others.none { it supports brick } }
+            return if (unsupported.isEmpty()) bricks else go(bricks + unsupported, others - bricks)
+        }
+
+        println("[${Thread.currentThread().name}] desintegrating $this ...")
+        return go(setOf(this), bricks.toSet() - this) - this
+    }
+
+    private infix fun overlapps(other: Brick): Boolean =
+        xs overlapps other.xs && ys overlapps other.ys
+
+}
+
+fun List<Brick>.settled(): List<Brick> {
+    val settledBricks = this.filter { it.zs.first == 1 }.toMutableList()
+    val fallingBricks = this.filter { it.zs.first != 1 }
+
+    fallingBricks.forEach { brick ->
+        val settledBrick = if (settledBricks.any { it supports brick }) {
+            brick
+        } else {
+            val maxZBelow = settledBricks.filter { it willSupport brick }.maxOfOrNull { it.zs.last } ?: 0
+            brick.moveTo(maxZBelow + 1)
+        }
+        settledBricks.filter { it supports settledBrick }.forEach { settledBrick.addSupporter(it) }
+        settledBricks.add(settledBrick)
+    }
+
+    return settledBricks
+}
+
+infix fun IntRange.overlapps(other: IntRange): Boolean =
+    other.last >= first && other.first <= last
+
+val IntRange.width: Int
+    get() = last - first
+
+fun IntRange.moveTo(x: Int) =
+    (x..x + width)
---
+++ kotlin/year2023/src/test/kotlin/day22/Day22Test.kt
@@ -0,0 +1,112 @@
+package day22
+
+import io.kotest.matchers.booleans.shouldBeFalse
+import io.kotest.matchers.booleans.shouldBeTrue
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+class Day22Test {
+
+    // 1,0,1~1,2,1   <- A
+    // 0,0,2~2,0,2   <- B
+    // 0,2,3~2,2,3   <- C
+    // 0,0,4~0,2,4   <- D
+    // 2,0,5~2,2,5   <- E
+    // 0,1,6~2,1,6   <- F
+    // 1,1,8~1,1,9   <- G
+    //
+    // Brick A is the only brick supporting bricks B and C.
+    // Brick B is one of two bricks supporting brick D and brick E.
+    // Brick C is the other brick supporting brick D and brick E.
+    // Brick D supports brick F.
+    // Brick E also supports brick F.
+    // Brick F supports brick G.
+    // Brick G isn't supporting any bricks.
+    private val sample = """
+        1,0,1~1,2,1
+        0,0,2~2,0,2
+        0,2,3~2,2,3
+        0,0,4~0,2,4
+        2,0,5~2,2,5
+        0,1,6~2,1,6
+        1,1,8~1,1,9
+    """.trimIndent()
+
+    @Test
+    fun `parse brick`() {
+        parseBrick("1,0,1~1,2,1") shouldBe Brick((1..1), (0..2), (1..1))
+    }
+
+    @Test
+    fun `overlapping ranges`() {
+        ((1..1) overlapps 0..2).shouldBeTrue()
+        ((0..2) overlapps 1..1).shouldBeTrue()
+
+        ((1..1) overlapps 2..2).shouldBeFalse()
+        ((2..2) overlapps 1..1).shouldBeFalse()
+    }
+
+    @Test
+    fun `settle bricks`() {
+        parseBricks(sample.lines()).settled() shouldBe bricksOf(
+            "1,0,1~1,2,1",
+            "0,0,2~2,0,2",
+            "0,2,2~2,2,2",
+            "0,0,3~0,2,3",
+            "2,0,3~2,2,3",
+            "0,1,4~2,1,4",
+            "1,1,5~1,1,6"
+        )
+    }
+
+    @Test
+    fun `link supporting and supported bricks`() {
+        val input = """
+            1,0,1~1,2,1
+            0,0,2~2,0,2
+            0,2,3~2,2,3
+            0,0,4~0,2,4
+        """.trimIndent()
+        val bricks = parseBricks(input.lines()).settled()
+
+        bricks.map(Brick::supporting) shouldBe listOf(
+            bricksOf("0,0,2~2,0,2", "0,2,2~2,2,2").toSet(),
+            bricksOf("0,0,3~0,2,3").toSet(),
+            bricksOf("0,0,3~0,2,3").toSet(),
+            bricksOf().toSet()
+        )
+
+        bricks.map(Brick::supportedBy) shouldBe listOf(
+            bricksOf().toSet(),
+            bricksOf("1,0,1~1,2,1").toSet(),
+            bricksOf("1,0,1~1,2,1").toSet(),
+            bricksOf("0,0,2~2,0,2", "0,2,2~2,2,2").toSet()
+        )
+    }
+
+    @Test
+    fun `solve part one with sample input`() {
+        val bricks = parseBricks(sample.lines())
+
+        solvePartOne(bricks) shouldBe 5
+    }
+
+    @Test
+    fun `solve part two with sample input`() {
+        val bricks = parseBricks(sample.lines())
+
+        solvePartTwo(bricks) shouldBe 7
+    }
+
+    @Test
+    fun `solve puzzle`() {
+        val solution = solvePuzzle(file)
+
+        solution.first shouldBe "522"
+        solution.second shouldBe "83519"
+    }
+
+}
+
+fun bricksOf(vararg inputs: String): List<Brick> =
+    parseBricks(inputs.toList())
