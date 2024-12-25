8f17903f9a638b66e74fc903729db9e6dd5d62e7
Stefan Scheidt <mail@stefanscheidt.de>
2024-12-25 10:04:07 +0100

    refactor: year 2024, day 25

---
+++ kotlin/year2024/src/main/kotlin/day25/Day25.kt
@@ -0,0 +1,34 @@
+package day25
+
+typealias Schematic = List<String>
+
+typealias Profile = List<Int>
+
+fun Profile.fits(other: Profile): Boolean =
+  this.zip(other).map { (a, b) -> a + b }.all { it <= 5 }
+
+fun Schematic.toProfile(): Profile {
+  val profile = MutableList(5) { 0 }
+  this.drop(1).forEach { line ->
+    line.forEachIndexed { index, char -> profile[index] += if (char == '#') 1 else 0 }
+  }
+  return profile
+}
+
+fun String.toSchematics(): Pair<List<Schematic>, List<Schematic>> =
+  split("\n\n").map(String::lines).partition { it.first() == "#####" }
+
+fun String.toProfiles(): Pair<List<Profile>, List<Profile>> {
+  val (keys, locks) = this.toSchematics()
+  val keyProfiles = keys.map(Schematic::toProfile)
+  val lockProfiles = locks.map(Schematic::reversed).map(Schematic::toProfile)
+  return keyProfiles to lockProfiles
+}
+
+// Part 1
+
+fun part1(input: String): String {
+  val (keys, locks) = input.toProfiles()
+  val fits = locks.sumOf { lock -> keys.count { key -> key.fits(lock) } }
+  return fits.toString()
+}
---
+++ kotlin/year2024/src/test/kotlin/day25/Day25Test.kt
@@ -0,0 +1,64 @@
+package day25
+
+import common.io.inputFile
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+class Day25Test {
+
+  private val sample =
+    """
+    #####
+    .####
+    .####
+    .####
+    .#.#.
+    .#...
+    .....
+
+    #####
+    ##.##
+    .#.##
+    ...##
+    ...#.
+    ...#.
+    .....
+
+    .....
+    #....
+    #....
+    #...#
+    #.#.#
+    #.###
+    #####
+
+    .....
+    .....
+    #.#..
+    ###..
+    ###.#
+    ###.#
+    #####
+
+    .....
+    .....
+    .....
+    #....
+    #.#..
+    #.#.#
+    #####
+    """
+      .trimIndent()
+
+  @Test
+  fun `solve part one with sample input`() {
+    part1(sample) shouldBe "3"
+  }
+
+  @Test
+  fun `solve part one`() {
+    val input = inputFile("day25.txt").readText().trimEnd()
+    part1(input) shouldBe "2824"
+  }
+
+}
