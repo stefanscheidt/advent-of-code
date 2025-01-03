ce19ba815f745550572456cb298dfae4f984d0b8
Stefan Scheidt <mail@stefanscheidt.de>
2025-01-03 18:34:01 +0100

    chore: update dependencies

--- kotlin/year2023/build.gradle.kts
+++ kotlin/year2023/build.gradle.kts
@@ -2,5 +2,5 @@ plugins { `aoc-kotlin-conventions` }
 
 dependencies {
   implementation(project(":common"))
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
 }
