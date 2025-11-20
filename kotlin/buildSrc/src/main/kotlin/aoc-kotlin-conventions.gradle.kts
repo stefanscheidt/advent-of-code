79db991dbad3b06f6b73e1a529d783bef1892d10
Stefan Scheidt <mail@stefanscheidt.de>
2025-11-20 12:03:57 +0100

    build(kotlin): update dependencies

--- kotlin/buildSrc/src/main/kotlin/aoc-kotlin-conventions.gradle.kts
+++ kotlin/buildSrc/src/main/kotlin/aoc-kotlin-conventions.gradle.kts
@@ -14,7 +14,7 @@ dependencies {
   implementation("com.github.h0tk3y.betterParse:better-parse:0.4.4")
 
   // https://docs.junit.org/current/user-guide/
-  testImplementation(platform("org.junit:junit-bom:6.0.0"))
+  testImplementation(platform("org.junit:junit-bom:6.0.1"))
   testImplementation("org.junit.jupiter:junit-jupiter")
   testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
@@ -22,7 +22,7 @@ dependencies {
   testImplementation(kotlin("test"))
 
   // https://kotest.io/docs/assertions/assertions.html
-  testImplementation("io.kotest:kotest-assertions-core:6.0.4")
+  testImplementation("io.kotest:kotest-assertions-core:6.0.5")
 
   // https://mockk.io/
   testImplementation("io.mockk:mockk:1.14.6")
